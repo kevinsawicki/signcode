38a81b764a6341002b4ea70f79d70887e7046a91
Kevin Sawicki <kevinsawicki@gmail.com>
2016-04-15 10:12:21 -0700

    Add initial signcode spawn helper

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
-const ChildProcess = require('child_process')
+var ChildProcess = require('child_process')
+var path = require('path')
 
-exports.createCertificate = function (options) {
-  const inputCertPath = options.inputCertPath
-  const outputCertPath = options.outputCertPath
-  const outputKeyPath = options.outputKeyPath
+exports.sign = function (options, callback) {
+  spawn(options, callback)
 }
 
+function spawn (options, callback) {
+  var args = [
+    '-nest',
+    '-in',
+    options.path,
+    '-out',
+    getOutputPath(options.path),
+    '-t',
+    'http://timestamp.verisign.com/scripts/timstamp.dll'
+  ]
 
-function createKeyPem () {
+  if (options.hash) {
+    args.push('-h', options.hash)
+  }
 
+  if (options.name) {
+    args.push('-n', options.name)
+  }
+
+  if (options.site) {
+    args.push('-i', options.site)
+  }
+
+  var signcode = ChildProcess.spawn(getSigncodePath(), args)
+  signcode.on('exit', function (code) {
+    if (code === 0) {
+      callback()
+    } else {
+      callback(Error('Signcode failed: ' + code))
+    }
+  })
 }
 
-function createCertPem () {
-
+function getOutputPath (inputPath) {
+  var extension = path.extname(inputPath)
+  var name = path.basename(inputPath, extension)
+  var outputName = name + '-signed' + extension
+  return path.join(path.dirname(inputPath), outputName)
 }
 
-function createKey () {
-
-}
-
-function createCert () {
-
+function getSigncodePath () {
+  return path.join(__dirname, vendor, process.platform, 'osslsigncode')
 }
