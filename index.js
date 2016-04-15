1f48e4ac0c7cfecbef749cd686c32fb5c5e9cbb4
Kevin Sawicki <kevinsawicki@gmail.com>
2016-04-15 09:57:34 -0700

    Vendor osslsigncode on Mac

---
+++ index.js
@@ -0,0 +1,24 @@
+const ChildProcess = require('child_process')
+
+exports.createCertificate = function (options) {
+  const inputCertPath = options.inputCertPath
+  const outputCertPath = options.outputCertPath
+  const outputKeyPath = options.outputKeyPath
+}
+
+
+function createKeyPem () {
+
+}
+
+function createCertPem () {
+
+}
+
+function createKey () {
+
+}
+
+function createCert () {
+
+}
